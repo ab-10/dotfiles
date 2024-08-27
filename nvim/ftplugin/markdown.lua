e1a8ad43f96ba24de73dc36a9a8caad05e56b84b
ab-10 <armins.bagrats@gmail.com>
2024-08-27 17:59:23 +0100

    Create an md foldexpr

--- nvim/ftplugin/markdown.lua
+++ nvim/ftplugin/markdown.lua
@@ -1,3 +1,19 @@
+_G.md_foldexpr = function ()
+    local line = vim.fn.getline(vim.v.lnum)
+    local header_level = vim.fn.matchstr(line, '^#*')
+
+    if #header_level == 1 then
+        return ">" .. tostring(#header_level)
+    end
+
+    return "="
+end
+
+
 local vim = vim
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "v:lua.md_foldexpr()"
+
+vim.cmd("set foldenable")
 
 vim.keymap.set('n', '<leader>mf', '<cmd>RenumberList<cr>', { desc = '[M]arkdown [F]ix list' })
